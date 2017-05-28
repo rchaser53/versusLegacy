35ce30b85116cb2f430588b891018cd064aa8538
rchaser53 <tayoshizawa29@gmail.com>
2017-05-28 14:24:21 +0900

    minimum sample code for sessionStorage

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+var path = require('path')
 var express = require('express')
 var app = express()
 
 app.use(express.static('./'))
+
 app.get('/', function(req, res){
   res.sendFile('index.html')
+})
+
+app.get('/1', function(req, res){
+  res.sendFile(path.resolve(__dirname, 'sessionTest', 'index1.html'))
+})
+
+app.get('/2', function(req, res){
+  res.sendFile(path.resolve(__dirname, 'sessionTest', 'index2.html'))
 })
 
 app.listen(3000, function(){
