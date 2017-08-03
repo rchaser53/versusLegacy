1edab37aca91e2bf68df4d518de41d2a3d156793
rchaser53 <tayoshizawa29@gmail.com>
2017-08-04 00:24:32 +0900

    try to use webpack in grunt task with color

---
+++ forWebpack/index.js
@@ -0,0 +1,13 @@
+const Vue = require('vue')
+
+new Vue({
+  el: '#app',
+  template: `
+    <div>123</div>
+  `,
+  data: () => {
+    return {
+      nyan: 'chu'
+    }
+  }
+})
--- grunt/webpack.js
+++ grunt/webpack.js
@@ -1,74 +1,20 @@
 const webpack = require('webpack')
 const options = require('../webpack.config.js')
-const { spawn } = require('child_process');
+const { spawn, spawnSync } = require('child_process');
 
 module.exports = function (grunt) {
   grunt.registerTask('webpack', function () {
     var done = this.async();
 
-    const ls = spawn('webpack', [], { stdio: ['ipc', null, null, null, 'pipe'] });
-    ls.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
-    });
+  try {
+    spawnSync('npm', ['run', 'webpack'], {
+      stdio: [1, 2, 3]
+    })
+  } catch (err) {
+    done(1);
+    return;
+  }
 
-    ls.stderr.on('data', (data) => {
-      console.log(`stderr: ${data}`);
-      done()
-    });
-
-    ls.on('close', (code) => {
-      done()
-    });
+  done();
   });
 };
-
-// try {
-// compiler = 
-// } catch(e) {
-  // var WebpackOptionsValidationError = require("../lib/WebpackOptionsValidationError");
-  // if(e instanceof WebpackOptionsValidationError) {
-  // 	if(argv.color)
-  // 		console.error("\u001b[1m\u001b[31m" + e.message + "\u001b[39m\u001b[22m");
-  // 	else
-  // 		console.error(e.message);
-  // 	process.exit(1); // eslint-disable-line no-process-exit
-  // }
-//   throw e;
-// }
-
-// if(argv.progress) {
-// 	var ProgressPlugin = require("../lib/ProgressPlugin");
-// 	compiler.apply(new ProgressPlugin({
-// 		profile: argv.profile
-// 	}));
-// }
-
-
-// function compilerCallback(err, stats) {
-//   if(!options.watch || err) {
-//     // Do not keep cache anymore
-//     compiler.purgeInputFileSystem();
-//   }
-
-//   if(err) {
-//     lastHash = null;
-//     console.error(err.stack || err);
-//     if(err.details) console.error(err.details);
-//     process.exit(1); // eslint-disable-line
-//   }
-
-//   // if(outputOptions.json) {
-//   // 	process.stdout.write(JSON.stringify(stats.toJson(outputOptions), null, 2) + "\n");
-//   // } else if(stats.hash !== lastHash) {
-//   // 	lastHash = stats.hash;
-//   // 	var statsString = stats.toString(outputOptions);
-//   // 	if(statsString)
-//   // 		process.stdout.write(statsString + "\n");
-//   // }
-
-//   if(!options.watch && stats.hasErrors()) {
-//     process.on("exit", function() {
-//       process.exit(2); // eslint-disable-line
-//     });
-//   }
-// }
